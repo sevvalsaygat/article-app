695000ec98745fdb77b6028279fc6aab089039c3
sevvalsaygat <sevvallsstt@gmail.com>
2022-07-27 23:41:13 +0300

    List data with axios

--- src/components/Articles/List/index.js
+++ src/components/Articles/List/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
-function index() {
+function Index({ articles, setArticles }) {
+
+  useEffect(() => {
+    axios
+      .get('https://fe-article-api.herokuapp.com/articles')
+      .then((res) => {
+        setArticles(res.data)
+      })
+  }, [])
+
   return (
-    <div>List</div>
+    <div>
+      <ul className='list'>
+        {
+          articles.map((article, i) => (
+            <li key={i}>
+              <span>{article.title}</span>
+              <span>{article.content}</span>
+            </li>
+          ))}
+      </ul>
+    </div>
   )
 }
 
-export default index
+export default Index
--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 import Form from './Form/index'
 import List from './List/index'
 
-function index() {
+function Index() {
+  const [articles, setArticles] = useState([])
+
   return (
     <div>
-      <List/>
-      <Form/>
+      <List
+        articles={articles}
+        setArticles={setArticles}
+      />
+      <Form />
     </div>
   )
 }
 
-export default index
+export default Index
