d4d930b36b7c94da1aade9d2d5be5b77438ab377
sevvalsaygat <sevvallsstt@gmail.com>
2022-07-27 23:06:17 +0300

    Create components structure

---
+++ src/components/Articles/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from './Form/index'
+import List from './List/index'
+
+function index() {
+  return (
+    <div>
+      <List/>
+      <Form/>
+    </div>
+  )
+}
+
+export default index
